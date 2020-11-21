87a0d30e3070b9e515e6768636cd3dc2e7374c33
saiteja352 <saitejayuvi173@gmail.com>
2020-11-21 20:44:00 +0530

    hai my new code

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import Header2 from './components/header2';
 import Home from './components/home';
 import Aboutus from './components/aboutus';
 import Contactus from './components/contactus';
-import {Switch,Route,BrowserRouter,Redirect} from "react-router-dom";
+import {Switch,Route,BrowserRouter} from "react-router-dom";
 
 class App extends React.Component{
   render(){
     return(
-       <BrowserRouter>
-       <Header/>
-       <Header2/>
-       <Switch> 
-       <Redirect exact from="/" to="/home" />     
-       <Route path="/home"  component={Home} />
-       <Route path="/aboutus" component={Aboutus}/>
-       <Route path="/contactus" component={Contactus}/>
-       </Switch>
+      <BrowserRouter>
+      <Header/>
+      <Header2/>
+      <Switch>
+        <Route path="/" component={Home}/>
+        <Route path="/aboutus" component={Aboutus}/>
+        <Route path="/contactus" component={Contactus}/>
+      </Switch>
       </BrowserRouter>
     )
   }
